54d85818b94722f941d514cb19557c427c8a30a3
xiaozhu7393 <739300585@qq.com>
2018-04-16 09:06:14 +0800

    mod by zkj @20180416

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-
+$(document).ready(function () {
+    $(selector).click(function (e) { 
+        e.preventDefault();
+        
+    });
+    $(selector).after(content);
+    $(selector).submit(function (e) { 
+        e.preventDefault();
+        
+    });
+    $(selector).attr(attributeName, value);
+    $(selector).attr(attributeName);
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: "url",
+        data: "data",
+        success: function (response) {
+            
+        }
+    });
+});
 
