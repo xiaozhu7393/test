ad3e6e6abb7e57bb08b21ab7e30c2b91f3e92e07
xiaozhu7393 <739300585@qq.com>
2018-04-16 14:13:01 +0800

    mod by zkj @20180416

--- js/main.js
+++ js/main.js
@@ -1,43 +1,55 @@
-$(function (){
-    (function  () {
-		$('body').on('click','.nav-toggle',function  (event) {
+var doc = document;
+$(function () {
+    (function () {
+        /** 响应式导航按钮 */
+        $('body').on('click', '.nav-toggle', function (event) {
             var id = $(this).attr("data-target");
-			var r = $(id).hasClass("nav-navicon");
-			if (r)
-			{
-				$(id).removeClass('nav-navicon');
-				$(this).addClass('listopen');
-			}
-			else
-			{
-				$(id).addClass('nav-navicon');
+            var r = $(id).hasClass("nav-navicon");
+            if (r) {
+                $(id).removeClass('nav-navicon');
+                $(this).addClass('listopen');
+            } else {
+                $(id).addClass('nav-navicon');
                 $(this).removeClass('listopen');
-			}
-			event.stopPropagation();
-		});
+            }
+            event.stopPropagation();
+        });
     })();
-    
-    (function (){
-        $('.nav-inline >li').click(function (e) { 
+    /** 响应式 二级导航 */
+    (function () {
+        $('.nav-inline >li').click(function (e) {
             var r = $(this).hasClass('active');
-            if (r){
+            if (r) {
                 $(this).removeClass('active');
-            }else{
+            } else {
                 $(this).addClass('active').siblings().removeClass('active');
             }
             //e.preventDefault();
         });
     })();
-    (function (){
-        $('.drop-menu li').click(function (e) { 
+    /** 响应式 三级导航 */
+    (function () {
+        $('.drop-menu li').click(function (e) {
             var r = $(this).hasClass('active');
-            if (r){
+            if (r) {
                 $(this).removeClass('active');
-            }else{
+            } else {
                 $(this).addClass('active').siblings().removeClass('active');
             }
             e.stopPropagation();
         });
     })();
-});
+    /** banner */
+    (function () {
+        var g_ban = doc.getElementById('g-ban');
+        if (g_ban) {
+            var mySwiper = new Swiper('.swiper-container', {
+                autoplay: 5000, //可选选项，自动滑动
+                loop: true, //可选选项，开启循环
+                speed: 1000
+            });
+        }
 
+    })();
+
+});
